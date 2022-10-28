cf57c4a7827e2f2c80fc4f7f197955f45943090f
katharina-spiecker <kathispiecker@gmail.com>
2022-10-28 22:14:30 +0200

    break down into smaller components

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,7 @@
                         <img :src="config.imageUrl">
                     </div>
                     <h5 class="left-col__headline" contenteditable="true" @blur="onFinishEditHeadline($event, 'personal')">{{config.headlines.personal}}</h5>
-                    <div class="d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
-                        <input class="pseudo-text-input w-100" type="text" v-model="config.contact.phone">
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-                        <input class="pseudo-text-input w-100" type="text" v-model="config.contact.email">
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
-                        <input class="pseudo-text-input w-100" type="text" v-model="config.contact.address">
-                    </div>
+                    <ContactSection :contact="config.contact" @contactInfoUpdated="updateContactInfo"/>
                 </div>
                 <div id="qualifications-section" class="mt-5">
                     <h5 class="left-col__headline" contenteditable="true" @blur="onFinishEditHeadline($event, 'qualifications')">{{config.headlines.qualifications}}</h5>
@@ -168,9 +157,11 @@
 
 <script>
 import html2pdf from "html2pdf.js";
+import ContactSection from "./components/ContactSection.vue";
 
 export default {
   name: 'App',
+  components: { ContactSection },
   data() {
         return {
             config: {
@@ -214,6 +205,7 @@ export default {
                         ],
                     },
                 ],
+
                 contact: {
                     phone: "015773909584",
                     email: "contact@gmail.com",
@@ -367,6 +359,9 @@ export default {
             enableLinks: true,
 
 		});
+      },
+      updateContactInfo(data){
+        this.config.contact[data.key] = data.newValue;
       }
   }
 }
